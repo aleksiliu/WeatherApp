5f927ed2308ce068374d3a954c325163cc42b5bc
Aleksi Liukkonen <aleksiliukkonen@Aleksis-MacBook-Pro.local>
2018-07-29 22:23:32 +0300

    Refactor code

--- main.js
+++ main.js
@@ -9,33 +9,45 @@ const h1Error = document.querySelector('.error');
 
 form.addEventListener("submit", function(e){
   e.preventDefault();
+  if (input.value.trim() === '') {
+    return false;
+  }
   getWeather();
 })
 
 function getWeather() {
+  button.textContent = 'Loading';
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&APPID=790b0cad877acccab6384572ca8dfa89`)
   .then(response => response.json())
   .then(function(data) {
-    displayWeather(data);
+    render(data);
   })
-  .catch(function(error) {
-    console.log(error);
+  .catch(function() {
+    button.textContent = 'Search';
+    showError('Something went wrong.');
   });   
 }
 
-function displayWeather(data) {
-  if (input.value === '') {
-    return false;
-  } else if (data.cod === '404') {
-    weather_div.style.display = 'none';
-    document.body.style.background = '#e74c3c';
-    h1Error.style.display = 'block';
-  } else {
+function showError(message) {
+  weather_div.style.display = 'none';
+  document.body.style.background = '#e74c3c';
+  h1Error.style.display = 'block';
+  h1Error.textContent = message;
+}
+
+function render(data) {
+  button.textContent = 'Search';
+  if (data.cod === 200) {
     weather_div.style.display = 'block';
     document.body.style.background = '#2ecc71';
     h1Error.style.display = 'none';
     h1.innerHTML = `${data.name}, ${data.sys.country}`;
     h2.innerHTML = `${data.main.temp} °C`;
     p.innerHTML = data['weather'][0]['main'];
+  } else if (data.cod === '404') {
+    showError('City not found.');
+  } else {
+    console.log(data);
+    showError('Something went wrong.');
   }
 }
